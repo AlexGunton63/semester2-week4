f58bbda0bb4a4ff4b9b0181136c203579416f9f7
AlexGunton63 <kbzz0907@leeds.ac.uk>
2026-03-03 14:43:06 +0000

    made for loop include value 0

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -12,7 +12,7 @@ int main( int argc, char **argv ) {
     // define appropriate data to hold your answer
     char outputStr[200] = "";
     // process the command-line data using appropriate string functions
-    for(int i = 0; i < argc; i++)
+    for(int i = 1; i < argc; i++)
     {
         strcat(outputStr,argv[i]);
         strcat(outputStr, "-");
